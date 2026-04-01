24e6b4e349f3495a3922ce1efe14bd6d13731839
2gub4 <jakub.sosna@o2.pl>
2026-04-01 14:31:56 +0200

    add new project CppPlayground for learning pusrposes

---
+++ CppPlayground/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main() {
+    std::cout<<"Welcome to ExcerciseHub. A place where you can access all my c++ projects and excercises\n";
+
+    int choice;
+    do {
+        std::cout<<"Choose valid option: "<<std::endl;
+        std::cout<<"\t1 - Main Function\t2 - Function output\n"
+                   "\t3 - Pointers\t 4 - IO\n\t5 - Classes"
+                   "\t6 - Revision\n\t7 - Compilation"
+                   "\n\t0 - Exit\n"
+        <<std::endl;
+        std::cin>>choice;
+        std::cout<<"Entering option "<<choice<<"\n\n"<<std::endl;
+    } while (choice != 0);
+    std::cout<<"Exiting program..."<<std::endl;
+}
