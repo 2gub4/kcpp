9dc2a8215b5964115fb2b9f7c8202653a979af87
2gub4 <jakub.sosna@o2.pl>
2026-04-01 15:40:58 +0200

    start string excercise

--- CppCourseExcercises/src/main.cpp
+++ CppCourseExcercises/src/main.cpp
@@ -1,6 +1,40 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <string>
+
+bool isStringEmpty(const std::string &in) {
+    return in.empty();
+}
+
+void stringDimensions(const std::string &in) {
+    std::cout<<"size of string: "<<in.size()<<"\n"
+    "length of string: "<<in.length()<<"\nlength from size: "
+    <<in.size()/8<<std::endl;
+}
+
+char stringAt(const std::string &in, int index) {
+    return in.at(index);
+}
+
+void clearString(const std::string &in) {
+    std::cout<<"initial string: "<<in<<std::endl;
+}
+
+std::string reverseString(std::string in) {
+    std::ranges::reverse(in.begin(), in.end());
+    return in;
+}
+
+void swapLetters(std::string a, std::string b) {
+    std::cout<<"initial strings: \na=\""<<a<<"\"\tb=\""<<b<<"\""<<std::endl;
+    std::swap(a, b);
+}
+
 
 int main() {
-    std::cout<<"Zadania C++!"<<std::endl;
+    std::cout<<"Zadania C++!\n\n"<<std::endl;
+    swapLetters("aaaaaaaa", "bbbbbbbb");
+    stringDimensions("bbbbbbbb");
     return 0;
 }
